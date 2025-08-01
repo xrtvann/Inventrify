2f4c0b2c2def401dada5ce1cf7d8623843a5e5f1
xrtvann <irvangoblas@gmail.com>
2025-08-01 22:42:36 +0700

    feat: enhance dashboard with detailed inventory and purchase summaries

--- public/pages/dashboard.php
+++ public/pages/dashboard.php
@@ -1,42 +1,97 @@
 <div class="container">
   <div class="grid grid-cols-3 gap-4">
     <div class="bg-white flex flex-col px-4 py-3 rounded-sm border border-gray-100 shadow-sm col-span-2">
-      <h2 class="font-sub-heading-medium text-greygrey-800 mb-4">Sales Overview</h2>
+      <h2 class="font-semibold text-greygrey-800 mb-4">Sales Overview</h2>
       <div class="flex justify-between">
         <div class="flex flex-col items-center w-full">
           <img class="w-[30px] h-[30px] mb-3" src="https://c.animaapp.com/mdrlomr79znNOH/img/sales.svg"
             alt="Sales icon" />
-            <p class="font-body-2-medium text-greygrey-500">Sales</p>
+          <p class="font-body-2-medium text-greygrey-500">Sales</p>
           <p class="font-body-1-semi-bold text-greygrey-600">Rp. 200.000</p>
         </div>
         <div class="flex flex-col items-center w-full">
           <img class="w-[30px] h-[30px] mb-3" src="https://c.animaapp.com/mdrlomr79znNOH/img/revenue.svg"
             alt="Revenue icon" />
-            <p class="font-body-2-medium text-greygrey-500">Revenue</p>
+          <p class="font-body-2-medium text-greygrey-500">Revenue</p>
           <p class="font-body-1-semi-bold text-greygrey-600">Rp. 1.000.000</p>
         </div>
         <div class="flex flex-col items-center w-full">
           <img class="w-[30px] h-[30px] mb-3" src="https://c.animaapp.com/mdrlomr79znNOH/img/profit.svg"
             alt="Profit icon" />
-            <p class="font-body-2-medium text-greygrey-500">Profit</p>
+          <p class="font-body-2-medium text-greygrey-500">Profit</p>
           <p class="font-body-1-semi-bold text-greygrey-600">Rp. 10.000.000</p>
         </div>
         <div class="flex flex-col items-center w-full">
           <img class="w-[30px] h-[30px] mb-3" src="https://c.animaapp.com/mdrlomr79znNOH/img/cost.svg"
             alt="Cost icon" />
-            <p class="font-body-2-medium text-greygrey-500">Cost</p>
+          <p class="font-body-2-medium text-greygrey-500">Cost</p>
           <p class="font-body-1-semi-bold text-greygrey-600">Rp. 100.000.000</p>
         </div>
       </div>
     </div>
     <div class="bg-white flex flex-col px-4 py-3 rounded-sm border border-gray-100 shadow-sm">
-      <p>Inventory overview</p>
+      <h2 class="font-semibold text-greygrey-800 mb-4">Inventory Summary</h2>
+      <div class="flex justify-between">
+        <div class="flex flex-col items-center w-full">
+          <img class="w-[30px] h-[30px] mb-3" src="https://c.animaapp.com/mdrlomr79znNOH/img/quantity.svg"
+            alt="Quantity icon" />
+          <p class="font-body-2-medium text-greygrey-500">Qty in hand</p>
+          <p class="font-body-1-semi-bold text-greygrey-600">868</p>
+        </div>
+        <div class="flex flex-col items-center w-full">
+          <img class="w-[30px] h-[30px] mb-3" src="https://c.animaapp.com/mdrlomr79znNOH/img/on-the-way.svg"
+            alt="On the way icon" />
+          <p class="font-body-2-medium text-greygrey-500">To be received</p>
+          <p class="font-body-1-semi-bold text-greygrey-600">235</p>
+        </div>
+
+      </div>
     </div>
     <div class="bg-white flex flex-col px-4 py-3 rounded-sm border border-gray-100 shadow-sm col-span-2">
-      <p>Purchase overview</p>
+      <h2 class="font-semibold text-greygrey-800 mb-4">Purchase Overview</h2>
+      <div class="flex justify-between">
+        <div class="flex flex-col items-center w-full">
+          <img class="w-[30px] h-[30px] mb-3" src="https://c.animaapp.com/mdrlomr79znNOH/img/purchase.svg"
+            alt="Purchase icon" />
+          <p class="font-body-1-semi-bold text-greygrey-600">82</p>
+          <p class="font-body-2-medium text-greygrey-500">Purchase</p>
+        </div>
+        <div class="flex flex-col items-center w-full">
+          <img class="w-[30px] h-[30px] mb-3" src="https://c.animaapp.com/mdrlomr79znNOH/img/cost.svg"
+            alt="Cost icon" />
+          <p class="font-body-1-semi-bold text-greygrey-600">₹ 13,573</p>
+          <p class="font-body-2-medium text-greygrey-500">Cost</p>
+        </div>
+        <div class="flex flex-col items-center w-full">
+          <img class="w-[30px] h-[30px] mb-3" src="https://c.animaapp.com/mdrlomr79znNOH/img/cancel.svg"
+            alt="Cancel icon" />
+          <p class="font-body-1-semi-bold text-greygrey-600">5</p>
+          <p class="font-body-2-medium text-greygrey-500">Cancel</p>
+        </div>
+        <div class="flex flex-col items-center w-full">
+          <img class="w-[30px] h-[30px] mb-3" src="https://c.animaapp.com/mdrlomr79znNOH/img/profit.svg"
+            alt="Return icon" />
+          <p class="font-body-1-semi-bold text-greygrey-600">₹17,432</p>
+          <p class="font-body-2-medium text-greygrey-500">Return</p>
+        </div>
+      </div>
     </div>
     <div class="bg-white flex flex-col px-4 py-3 rounded-sm border border-gray-100 shadow-sm">
-      <p>Product summary</p>
+      <h2 class="font-semibold text-greygrey-800 mb-4">Product Summary</h2>
+      <div class="flex justify-between">
+        <div class="flex flex-col items-center w-full">
+          <img class="w-[30px] h-[30px] mb-3" src="https://c.animaapp.com/mdrlomr79znNOH/img/suppliers-1.svg"
+            alt="Suppliers icon" />
+          <p class="font-body-2-medium text-greygrey-500">Suppliers</p>
+          <p class="font-body-1-semi-bold text-greygrey-600">30</p>
+        </div>
+        <div class="flex flex-col items-center w-full">
+          <img class="w-[30px] h-[30px] mb-3" src="https://c.animaapp.com/mdrlomr79znNOH/img/categories.svg"
+            alt="Categories icon" />
+          <p class="font-body-2-medium text-greygrey-500">Category</p>
+          <p class="font-body-1-semi-bold text-greygrey-600">97</p>
+        </div>
+      </div>
     </div>
     <div class="bg-white flex flex-col px-4 py-3 rounded-sm border border-gray-100 shadow-sm col-span-2">
       <p>Sales & purchase</p>
