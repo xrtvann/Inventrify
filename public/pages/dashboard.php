0566a43022ba4f05f22c9ad58234d9ff68f9a44a
xrtvann <irvangoblas@gmail.com>
2025-08-01 21:28:53 +0700

    feat: update dashboard layout with new sales overview section and icons

--- public/pages/dashboard.php
+++ public/pages/dashboard.php
@@ -1,92 +1,54 @@
 <div class="container">
-    <div class="grid grid-cols-3 gap-4">
-        <div class="bg-white flex flex-col px-4 py-3 rounded-sm border border-gray-100 shadow-sm col-span-2">
-            <div class="title-card mb-4">
-                <h3 class="text-gray-800 font-semibold tracking-wide">Sales overview</h3>
-            </div>
-            <div class="component-overview grid grid-cols-4 gap-1">
-                <div class="sales p-2">
-                    <div class="icon flex items-center justify-center">
-                        <div class="wrap bg-blue-100 p-2 flex items-center justify-center rounded-sm">
-                            <i class="fas fa-chart-line text-lg font-light text-blue-600"></i>
-                        </div>
-                    </div>
-                    <div class="text-content mt-2 flex justify-between items-center">
-                        <div class="nominal tabular-nums">
-                            <p>Rp. 10.000.000</p>
-                        </div>
-                        <div class="info">
-                            <p>Sales</p>
-                        </div>
-                    </div>
-                </div>
-                <div class="sales bg-red-500">
-                    <div class="icon flex items-center justify-center">
-                        <div class="wrap bg-blue-100 p-2 flex items-center justify-center rounded-sm">
-                            <i class="fas fa-chart-line text-lg font-light text-blue-600"></i>
-                        </div>
-                    </div>
-                    <div class="text-content flex justify-between items-center">
-                        <div class="nominal">
-                            <p>Rp. 10.000.000</p>
-                        </div>
-                        <div class="info">
-                            <p>Sales</p>
-                        </div>
-                    </div>
-                </div>
-                <div class="sales bg-red-500">
-                    <div class="icon flex items-center justify-center">
-                        <div class="wrap bg-blue-100 p-2 flex items-center justify-center rounded-sm">
-                            <i class="fas fa-chart-line text-lg font-light text-blue-600"></i>
-                        </div>
-                    </div>
-                    <div class="text-content flex justify-between items-center">
-                        <div class="nominal">
-                            <p>Rp. 10.000.000</p>
-                        </div>
-                        <div class="info">
-                            <p>Sales</p>
-                        </div>
-                    </div>
-                </div>
-                <div class="sales bg-red-500">
-                    <div class="icon flex items-center justify-center">
-                        <div class="wrap bg-blue-100 p-2 flex items-center justify-center rounded-sm">
-                            <i class="fas fa-chart-line text-lg font-light text-blue-600"></i>
-                        </div>
-                    </div>
-                    <div class="text-content flex justify-between items-center">
-                        <div class="nominal">
-                            <p>Rp. 10.000.000</p>
-                        </div>
-                        <div class="info">
-                            <p>Sales</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
+  <div class="grid grid-cols-3 gap-4">
+    <div class="bg-white flex flex-col px-4 py-3 rounded-sm border border-gray-100 shadow-sm col-span-2">
+      <h2 class="font-sub-heading-medium text-greygrey-800 mb-4">Sales Overview</h2>
+      <div class="flex justify-between">
+        <div class="flex flex-col items-center w-full">
+          <img class="w-[30px] h-[30px] mb-3" src="https://c.animaapp.com/mdrlomr79znNOH/img/sales.svg"
+            alt="Sales icon" />
+            <p class="font-body-2-medium text-greygrey-500">Sales</p>
+          <p class="font-body-1-semi-bold text-greygrey-600">Rp. 200.000</p>
         </div>
-        <div class="bg-white flex flex-col px-4 py-3 rounded-sm border border-gray-100 shadow-sm">
-            <p>Inventory overview</p>
+        <div class="flex flex-col items-center w-full">
+          <img class="w-[30px] h-[30px] mb-3" src="https://c.animaapp.com/mdrlomr79znNOH/img/revenue.svg"
+            alt="Revenue icon" />
+            <p class="font-body-2-medium text-greygrey-500">Revenue</p>
+          <p class="font-body-1-semi-bold text-greygrey-600">Rp. 1.000.000</p>
         </div>
-        <div class="bg-white flex flex-col px-4 py-3 rounded-sm border border-gray-100 shadow-sm col-span-2">
-            <p>Purchase overview</p>
+        <div class="flex flex-col items-center w-full">
+          <img class="w-[30px] h-[30px] mb-3" src="https://c.animaapp.com/mdrlomr79znNOH/img/profit.svg"
+            alt="Profit icon" />
+            <p class="font-body-2-medium text-greygrey-500">Profit</p>
+          <p class="font-body-1-semi-bold text-greygrey-600">Rp. 10.000.000</p>
         </div>
-        <div class="bg-white flex flex-col px-4 py-3 rounded-sm border border-gray-100 shadow-sm">
-            <p>Product summary</p>
+        <div class="flex flex-col items-center w-full">
+          <img class="w-[30px] h-[30px] mb-3" src="https://c.animaapp.com/mdrlomr79znNOH/img/cost.svg"
+            alt="Cost icon" />
+            <p class="font-body-2-medium text-greygrey-500">Cost</p>
+          <p class="font-body-1-semi-bold text-greygrey-600">Rp. 100.000.000</p>
         </div>
-        <div class="bg-white flex flex-col px-4 py-3 rounded-sm border border-gray-100 shadow-sm col-span-2">
-            <p>Sales & purchase</p>
-        </div>
-        <div class="bg-white flex flex-col px-4 py-3 rounded-sm border border-gray-100 shadow-sm">
-            <p>Order summary</p>
-        </div>
-        <div class="bg-white flex flex-col px-4 py-3 rounded-sm border border-gray-100 shadow-sm col-span-2">
-            <p>Top selling stock</p>
-        </div>
-        <div class="bg-white flex flex-col px-4 py-3 rounded-sm border border-gray-100 shadow-sm">
-            <p>Low quantity stock</p>
-        </div>
+      </div>
     </div>
+    <div class="bg-white flex flex-col px-4 py-3 rounded-sm border border-gray-100 shadow-sm">
+      <p>Inventory overview</p>
+    </div>
+    <div class="bg-white flex flex-col px-4 py-3 rounded-sm border border-gray-100 shadow-sm col-span-2">
+      <p>Purchase overview</p>
+    </div>
+    <div class="bg-white flex flex-col px-4 py-3 rounded-sm border border-gray-100 shadow-sm">
+      <p>Product summary</p>
+    </div>
+    <div class="bg-white flex flex-col px-4 py-3 rounded-sm border border-gray-100 shadow-sm col-span-2">
+      <p>Sales & purchase</p>
+    </div>
+    <div class="bg-white flex flex-col px-4 py-3 rounded-sm border border-gray-100 shadow-sm">
+      <p>Order summary</p>
+    </div>
+    <div class="bg-white flex flex-col px-4 py-3 rounded-sm border border-gray-100 shadow-sm col-span-2">
+      <p>Top selling stock</p>
+    </div>
+    <div class="bg-white flex flex-col px-4 py-3 rounded-sm border border-gray-100 shadow-sm">
+      <p>Low quantity stock</p>
+    </div>
+  </div>
 </div>
