21fb22e35b78953e90ce4a36290b92cb905425a8
xrtvann <irvangoblas@gmail.com>
2025-08-02 21:34:04 +0700

    feat: add product search and action buttons to products page

---
+++ public/pages/products.php
@@ -0,0 +1,25 @@
+<div class="container bg-white px-5 py-4 rounded-sm shadow-md">
+    <div class="flex justify-between">
+        <div class="search-box">
+            <form action="" method="post" class="relative">
+                <i class="fa fa-search text-sm absolute top-3 left-3 text-slate-500"></i>
+                <input type="text"
+                    class="search-input focus:outline-none border border-slate-300 focus:border-slate-500 bg-slate-100 px-6 py-1.5 ps-10 rounded-sm"
+                    placeholder="Search products..." />
+            </form>
+        </div>
+        <div class="right-action-buttons flex items-center gap-3">
+            <div class="add-product">
+                <button class="bg-blue-600 text-white px-4 py-1.5 rounded-sm hover:bg-blue-700 cursor-pointer transition duration-200">
+                    Add Product
+                </button>
+            </div>
+            <div class="filter">
+                <button class="text-slate-600 px-4 py-1.5 rounded-sm border border-slate-300 cursor-pointer hover:bg-slate-500 hover:text-white transition duration-200">
+                    <i class="fa fa-filter"></i>
+                    Filter
+                </button>
+            </div>
+        </div>
+    </div>
+</div>
